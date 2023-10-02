f24acc8fad1e2a9f27d9bbad7e81628d6c0233c5
giugabriella <giuh.gabriella@gmail.com>
2023-10-02 20:58:19 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/victor/_usjt_psc_interface_grafica/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.victor._usjt_psc_interface_grafica;
+
+/**
+ *
+ * @author 823125249
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
